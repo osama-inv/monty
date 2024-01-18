c46a1a3dcb11e076c8dceb2607ac6544aabd6ff6
belal <belalismail252@gmail.com>
2024-01-18 01:47:38 -0800

    pint file

---
+++ monty_pint.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * monf_pint - is a function that prints the top
+ *	of the stack, followed by a new line
+ *
+ * @head: is the head of the stack
+ * @counter: is the line number
+ *
+ * Return: void
+*/
+
+void monf_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
